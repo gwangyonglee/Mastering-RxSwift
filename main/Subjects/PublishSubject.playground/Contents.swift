32f8414cc4830513365ae7e2d1275fdb65952933
gwangyonglee <gwangyonglee92@gmail.com>
2019-09-14 20:56:01 +0900

    PublishSubject 예제코드 추가

--- main/Subjects/PublishSubject.playground/Contents.swift
+++ main/Subjects/PublishSubject.playground/Contents.swift
@@ -34,3 +34,32 @@ enum MyError: Error {
    case error
 }
 
+// 문자열이 포함된 Next이벤트를 받아서 다른 옵저버에게 전달
+let subject = PublishSubject<String>()
+subject.onNext("Hello")
+
+// 새로운 옵저버를 추가하고 Subject를 subscribe
+let o1 = subject.subscribe { print(">> 1", $0) }
+o1.disposed(by: disposeBag)
+
+// PublishSubject로 Next이벤트 전달
+subject.onNext("RxSwift")
+
+// 새로운 옵저버 추가
+let o2 = subject.subscribe { print(">> 2", $0) }
+o2.disposed(by: disposeBag)
+
+// 두 구독자에게 이벤트 전달
+subject.onNext("RxSwift2")
+
+// Subject로 Completed 이벤트 전달
+subject.onCompleted()
+
+// Subject로 Error 이벤트 전달
+//subject.onError(MyError.error)
+
+// Subject가 완료된 이후에 새로운 구독자 추가
+let o3 = subject.subscribe { print(">> 3", $0) }
+o3.disposed(by: disposeBag)
+
+// Observable에서 Completed가 호출된 이후 더 이상 Next이벤트가 전달되지 않는다. Subject도 동일
