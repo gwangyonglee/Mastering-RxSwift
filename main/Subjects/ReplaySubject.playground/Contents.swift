1f0564c35a3d46c514ef479fc82ed680806e0185
gwangyonglee <gwangyonglee92@gmail.com>
2019-09-23 21:52:39 +0900

    ReplaySubject 예제코드 추가

--- main/Subjects/ReplaySubject.playground/Contents.swift
+++ main/Subjects/ReplaySubject.playground/Contents.swift
@@ -34,4 +34,55 @@ enum MyError: Error {
    case error
 }
 
+// 생성자가 아닌 create메소드로 생성
+// 버퍼의 크기를 3으로 지정하면 3개의 이벤트를 저장하는 버퍼가 생성됨
+let replaySubject = ReplaySubject<Int>.create(bufferSize: 3)
 
+// 10개의 Next 이벤트 전달
+(1...10).forEach { replaySubject.onNext($0) }
+
+// Subject 구독
+replaySubject.subscribe { print("Observer 1 >>", $0) }
+	.disposed(by: disposeBag)
+
+// 마지막에 전달된 3개 출력
+// Observer 1 >> next(8)
+// Observer 1 >> next(9)
+// Observer 1 >> next(10)
+
+replaySubject.subscribe { print("Observer 2 >>", $0) }
+	.disposed(by: disposeBag)
+
+replaySubject.onNext(11)
+// 버퍼에서 가장 오래된 이벤트가 삭제. 즉, 8이 삭제
+
+replaySubject.subscribe { print("Observer 3 >>", $0) }
+	.disposed(by: disposeBag)
+
+replaySubject.onCompleted()
+
+// Observer 1 >> next(8)
+// Observer 1 >> next(9)
+// Observer 1 >> next(10)
+// Observer 2 >> next(8)
+// Observer 2 >> next(9)
+// Observer 2 >> next(10)
+// Observer 1 >> next(11)
+// Observer 2 >> next(11)
+// Observer 3 >> next(9)
+// Observer 3 >> next(10)
+// Observer 3 >> next(11)
+// Observer 1 >> completed
+// Observer 2 >> completed
+// Observer 3 >> completed
+
+replaySubject.subscribe { print("Observer 4 >>", $0) }
+	.disposed(by: disposeBag)
+
+// 버퍼에 저장되어있는 이벤트가 전달된 다음에 Completed이벤트 전달 
+// Observer 4 >> next(9)
+// Observer 4 >> next(10)
+// Observer 4 >> next(11)
+// Observer 4 >> completed
+
+// 종료여부와 관계없이 버퍼에 저장되어있는 이벤트를 새로운 구독자에게 전달
